45ad3387a2b0354154df383a49b6e6b0f83779e3
Javier Atalah <javier.atalah@cawthron.org.nz>
2019-03-14 22:39:15 +0100

    nuevos indices

--- index_calculator_func.R
+++ index_calculator_func.R
@@ -1,22 +1,93 @@
-index_calculator <-
+# rescale variables between 0 and 1
+range_0_1 <-
+  function(x) {
+    (x - min(x, na.rm = T)) / (max(x, na.rm = T) - min(x, na.rm = T)) * 1
+  }
+
+
+# 01 CS  index calculator---
+index_calculator_cs <-
   function(data) {
     data %>%
       gather(Species, abund, -Cod) %>%
-      left_join(scores, by = 'Species') %>%
+      group_by(Cod) %>%
+      mutate(N = sum(abund),
+             H = diversity(abund),
+             S = specnumber(abund)) %>%
+      left_join(biomass %>%
+                  gather(Species, biomass, -Cod) %>%
+                  group_by(Cod) %>%
+                  mutate(B = sum(biomass)),
+                by = 'Cod') %>%
+      group_by(Cod) %>%
+      summarise(
+        N = mean(N),
+        S = mean(S),
+        B = mean(B),
+        H = mean(H)
+      ) %>%
+      ungroup() %>%
+      mutate(
+        N_std = range_0_1(N),
+        S_std = range_0_1(S),
+        B_std = range_0_1(B),
+        H_std = range_0_1(H)
+      ) %>%
+      transmute(Cod = Cod,
+                cc_index = ((N_std + S_std + B_std + H_std) / 4) * 100)
+  }
+
+# 02 Trophic level index calculator --------------
+index_calculator_TL <-
+  function(data) {
+    data %>%
+      gather(Species, biomass, -Cod) %>%
+      group_by(Cod) %>%
+      left_join(trophic_level, by = 'Species') %>%
+      group_by(Cod) %>%
+      mutate(troph_biomass = troph * biomass,
+             tot_biomass = sum(biomass)) %>%
+      summarise(TL_index = sum(troph_biomass) / first(tot_biomass))
+  }
+
+# 03 vulnerability index-----
+index_calculator_vul <-
+  function(data) {
+    data %>%
+      gather(Species, abund, -Cod) %>%
+      left_join(scores_vul, by = 'Species') %>%
       group_by(Cod) %>%
       mutate(N = sum(abund)) %>%
-      group_by(Cod, local_imp) %>%
+      group_by(Cod, vuner) %>%
       mutate(prop_n = sum(abund) / N) %>%
       summarise(n = mean(prop_n, na.rm = T) * 100) %>%
       gather(key, value, n) %>%
-      unite(local_imp_comb, local_imp, key) %>%
-      spread(local_imp_comb, value) %>%
-      mutate(imp_index   = (1.5 * I_n + 3 * II_n + 4.5 * III_n + 6 * IV_n) / 100) %>% 
-      full_join(factores, by = 'Cod') %>%
-      mutate(cat_index = cut(
-        imp_index,
-        breaks = quantile(.$imp_index, probs = seq(0, 1, 0.25)),
-        labels = c('Low', 'Medium', 'High', 'Very high'),
-        include.lowest = T
-      ))
+      unite(vuner_comb, vuner, key) %>%
+      spread(vuner_comb, value) %>%
+      transmute(vul_index   = (0 * I_n + 1.5 * II_n + 3 * III_n) / 100)
+  }
+
+
+# 04 fishing resource index calculator----------
+index_calculator_fishing <-
+  function(data) {
+    data %>%
+      gather(Species, abund, -Cod) %>%
+      left_join(scores_fishing, by = 'Species') %>%
+      group_by(Cod) %>%
+      mutate(N = sum(abund)) %>%
+      group_by(Cod, fish_resource) %>%
+      mutate(prop_n = sum(abund) / N) %>%
+      summarise(n = mean(prop_n, na.rm = T) * 100) %>%
+      gather(key, value, n) %>%
+      unite(fish_resource_comb, fish_resource, key) %>%
+      spread(fish_resource_comb, value) %>%
+      transmute(fishing_index   = (0 * I_n + 1 * II_n + 2 * III_n + 3 * IV_n) / 100) 
+    # full_join(factores, by = 'Cod') %>%
+    # mutate(cat_index = cut(
+    #   fishing_index,
+    #   breaks = quantile(.$fishing_index, probs = seq(0, 1, 0.25)),
+    #   labels = c('Low', 'Medium', 'High', 'Very high'),
+    #   include.lowest = T
+    # ))
   }
--- indices.R
+++ indices.R
@@ -1,90 +1,56 @@
+# load libraries
 library(tidyverse)
 library(readxl)
-library(rfishbase)
 library(vegan)
+rm(list = ls())
 
-# read data------------
-abundance <- read_excel('ar_fish_data.xlsx', 1)
+# 01 read data------------
+abundance <- read_excel('ar_fish_data.xlsx', sheet = 'abundance')
 
 # biomass data
 biomass <- read_excel('ar_fish_data.xlsx', 2)
 
 # score data
-scores <-
-  read_excel('ar_fish_data.xlsx', 3) %>%
-  dplyr::select(Species, local_imp)
+scores_fishing <-
+  read_excel('ar_fish_data.xlsx', sheet = 'scores') %>%
+  dplyr::select(Species, fish_resource)
 
-# factors-----
+# score vulnerability
+scores_vul <-
+  read_excel('ar_fish_data.xlsx', sheet = 'scores') %>%
+  dplyr::select(Species, vuner)
+
+# trophic level data
+trophic_level <- 
+  read_excel('ar_fish_data.xlsx', sheet = 'scores') %>%
+  dplyr::select(Species, troph)
+  
+# factors
 factores <-  read_excel('ar_fish_data.xlsx', 4)
 
-# get fishase ecological parameters--------
-fisbase_data <-
-  scores %>%
-  mutate(estimates = map(Species, rfishbase::estimate)) %>%
-  select(estimates) %>%
-  flatten_df() %>%
-  select(Species, MaxLengthTL, Troph, a , K) %>%
-  write_csv('fishbase_parameters.csv')
-
-# check taxonomy------
-# library(worrms)
-# worm_names <-
-#   scores %>%
-#   mutate(estimates = map(Species, wm_records_taxamatch))
-
-
-# 2 Indices calculations#
-
-# Abundace----------
+# load index calculator functions-----
 source('index_calculator_func.R')
 
-index_abundance <- index_calculator(abundance)
-# plots ----------------
-breaks_abundance <-
-  data.frame(y = quantile(index_abundance$imp_index, probs = seq(0, 1, 0.25)))
+# 2 Indices calculations-----------
 
+# 2.1 Community structure index-------
+cc_index <- index_calculator_cs(abundance)
 
-ggplot(index_abundance, aes(x = Season, y = imp_index)) +
-  geom_boxplot() +
-  theme_bw() +
-  geom_point(aes(color= cat_index), size = 3, position = position_jitter()) +
-  geom_hline(aes(yintercept = y),breaks_abundance, lty = 2) +
-  scale_color_viridis_d(name = 'Importance \n category') +
-  labs(y = "Abundance index of importance")
+# 2.2 Trophic level--------------
+TL_index <- index_calculator_TL(biomass)
 
+# 2.3 Vulnerability index-------
+vuln_index <- index_calculator_vul(abundance)
 
-ggplot(index_abundance, aes(x = Year, y = imp_index, color = Season)) +
-  geom_smooth() +
-  geom_point(size = 3, position = position_jitter()) +
-  geom_hline(aes(yintercept = y),breaks_abundance, lty = 2) +
-  scale_color_discrete(name = 'Importance \n category') +
-  theme_bw() +
-  labs(y = "Abundance index of importance")
+# 2.4 Fishing resource index----------
+fishing_resource_index <- index_calculator_fishing(abundance)
 
-# Biomass ----------------
-index_biomass <- index_calculator(biomass)
-
-breaks_biomass <-
-  data.frame(y = quantile(index_biomass$imp_index, probs = seq(0, 1, 0.25)))
-
-ggplot(index_biomass, aes(x = Season, y = imp_index)) +
-  geom_boxplot() +
-  theme_bw() +
-  geom_point(aes(color= cat_index), size = 3, position = position_jitter()) +
-  geom_hline(aes(yintercept = y),breaks_biomass, lty = 2) +
-  scale_color_viridis_d(name = 'Importance \n category') +
-  labs(y = "Biomass index of importance")
-
-
-ggplot(index_biomass, aes(x = Year, y = imp_index, color = Season)) +
-  geom_smooth(se = F) +
-  geom_point(size = 3, position = position_jitter()) +
-  geom_hline(aes(yintercept = y),breaks_biomass, lty = 2) +
-  scale_color_discrete(name = 'Importance \n category') +
-  theme_bw() +
-  labs(y = "Biomass index of importance")
-
-ggplot(index_biomass, aes(x = Year, y = imp_index, color = Season)) +
-  stat_summary(fun.data = 'mean_se')
-
+# 03 Combine all indices and save as .csv file  ---------
+all_indices <-
+  full_join(cc_index, TL_index, by = "Cod") %>%
+  full_join(vuln_index, by = "Cod") %>%
+  full_join(fishing_resource_index, by = "Cod") %>%
+  mutate(ARI =  (cc_index*.25) + (TL_index*.25) + (vul_index*.25) + (fishing_index*.25)) %>%
+  print(n = 10) %>% 
+  write_csv('indices.csv')
 
